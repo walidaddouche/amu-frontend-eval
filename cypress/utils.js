1960357ba9c7df48b4f39d39f48037a9c2120f1b
Lior Chamla <lchamla@gmail.com>
2023-03-03 09:20:35 +0000

    correction cypress

--- cypress/utils.js
+++ cypress/utils.js
@@ -1,9 +1,10 @@
 // L'URL sur laquelle votre application web est visitable (à changer si nécessaire)
 export const BASE_URL = "http://localhost:4200/";
 // L'URL de l'API Supabase à mettre à jour absolument
-export const API_URL = "https://IDENTIFANT_SUPABASE.supabase.co/rest/v1";
+export const API_URL = "https://gdzprwugmxdxnpgztbwt.supabase.co/rest/v1";
 // La clé d'API de votre compte Supabase à mettre à jour absolument
-export const API_KEY = "CLE_API_SUPABASE";
+export const API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDUyMjE4MCwiZXhwIjoxOTU2MDk4MTgwfQ.bs4zHK_Hor5Z1ojmZ9MBF_jTvZpiye_DpiUPZcq209A";
 
 /**
  * Petite fonction utilitaire qui permet de supprimer tout ce qui se trouve dans les tables customers et invoices
@@ -12,7 +13,7 @@ export const API_KEY = "CLE_API_SUPABASE";
 export const resetDatabase = () => {
   cy.request({
     method: "DELETE",
-    url: API_URL + "/invoices",
+    url: API_URL + "/invoices?id=gt.0",
     headers: {
       apiKey: API_KEY,
     },
@@ -20,7 +21,7 @@ export const resetDatabase = () => {
 
   cy.request({
     method: "DELETE",
-    url: API_URL + "/customers",
+    url: API_URL + "/customers?id=gt.0",
     headers: {
       apiKey: API_KEY,
     },
